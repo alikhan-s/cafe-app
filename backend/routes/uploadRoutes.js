eeff1f48c8f962a8da6a8e85f4701c16abef6f00
Alikhan Serik <serikalikhan870@gmail.com>
2026-03-10 21:28:46 +0500

    feat(uploading image)

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,41 +1,32 @@
-const path = require('path');
 const express = require('express');
 const multer = require('multer');
+const { v2: cloudinary } = require('cloudinary');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+require('dotenv').config();
+
 const router = express.Router();
 
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(
-            null,
-            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-        );
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    params: {
+        folder: 'cafe-menu',
+        allowed_formats: ['jpg', 'jpeg', 'png', 'webp'],
     },
 });
 
-function checkFileType(file, cb) {
-    const filetypes = /jpg|jpeg|png|webp/;
-    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-    const mimetype = filetypes.test(file.mimetype);
-
-    if (extname && mimetype) {
-        return cb(null, true);
-    } else {
-        cb('Images only!');
-    }
-}
-
-const upload = multer({
-    storage,
-    fileFilter: function (req, file, cb) {
-        checkFileType(file, cb);
-    },
-});
+const upload = multer({ storage });
 
 router.post('/', upload.single('image'), (req, res) => {
-    res.send(`/${req.file.path.replace(/\\/g, '/')}`);
+    if (!req.file) {
+        return res.status(400).json({ message: 'The file was not downloaded.' });
+    }
+    res.status(200).send(req.file.path);
 });
 
 module.exports = router;
